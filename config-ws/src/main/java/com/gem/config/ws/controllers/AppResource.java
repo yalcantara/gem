45f0488970706a556b70582a2f6f3162ee64c095
Yaison <yaison1708@gmail.com>
2019-03-12 01:08:07 -0400

    Using more often the Query api.

--- config-ws/src/main/java/com/gem/config/ws/controllers/AppResource.java
+++ config-ws/src/main/java/com/gem/config/ws/controllers/AppResource.java
@@ -27,65 +27,65 @@ import com.gem.config.ws.services.AppService;
 @Component
 @Path("/apps")
 public class AppResource {
-
+	
 	@Inject
 	private AppService srv;
-	
+
 	@Context
 	private UriInfo info;
-	
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response list() {
-		
+
 		List<App> list = srv.list();
-		
+
 		return Response.ok(list).build();
 	}
-	
+
 	@GET
 	@Path("/{name}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response get(@PathParam("name") String name) {
-		
+
 		App app = srv.get(name);
-		
+
 		return Response.ok(app).build();
 	}
-
+	
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response create(App data) {
-
+		
 		App app = srv.create(data);
 		String name = app.getName();
-		
+
 		return Response.created(location(name)).build();
 	}
-	
+
 	@PUT
 	@Path("/{name}")
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response update(@PathParam("name") String name, App data) {
-		
-		TxResult<App> tx = srv.put(name, data);
 
+		TxResult<App> tx = srv.put(name, data);
+		
 		if (tx.isCreated()) {
 			String ans = tx.getResult().getName();
 			return Response.created(location(ans)).build();
 		}
-		
+
 		return Response.ok().build();
 	}
-
+	
 	@DELETE
 	@Path("/{name}")
 	public Response delete(@PathParam("name") String name) {
-
+		
 		boolean ans = srv.delete(name);
-		return Response.ok(String.valueOf(ans)).build();
+		return Response.ok(ans).build();
 	}
-
+	
 	private URI location(String name) {
 		UriBuilder b = info.getAbsolutePathBuilder();
 		b.path(name);
--- config-ws/src/main/java/com/gem/config/ws/entities/Property.java
+++ config-ws/src/main/java/com/gem/config/ws/entities/Property.java
@@ -3,91 +3,65 @@ package com.gem.config.ws.entities;
 import java.io.Serializable;
 import java.util.Date;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 /**
  * The persistent class for the properties database table.
  *
  */
 @Entity
-@Table(name = "properties")
 public class Property implements Serializable {
-
+	
 	private static final long serialVersionUID = -5182715756926792735L;
-	
-	@Id
-	private Long id;
-	
-	@ManyToOne
-	private App app;
 
 	private String name;
-
+	
 	private String label;
 
-	@Temporal(TemporalType.TIMESTAMP)
-	@Column(name = "creation_date")
 	private Date creationDate;
-
-	@Temporal(TemporalType.TIMESTAMP)
-	@Column(name = "last_update")
+	
 	private Date lastUpdate;
-
+	
 	public Property() {
-	}
-
-	public Long getId() {
-		return this.id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
+		super();
 	}
 
 	public Date getCreationDate() {
 		return this.creationDate;
 	}
-
+	
 	public void setCreationDate(Date creationDate) {
 		this.creationDate = creationDate;
 	}
-
+	
 	public String getLabel() {
 		return this.label;
 	}
-
+	
 	public void setLabel(String label) {
 		this.label = label;
 	}
-
+	
 	public Date getLastUpdate() {
 		return this.lastUpdate;
 	}
-
+	
 	public void setLastUpdate(Date lastUpdate) {
 		this.lastUpdate = lastUpdate;
 	}
-
+	
 	public String getName() {
 		return this.name;
 	}
-
+	
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	public App getApp() {
-		return this.app;
-	}
-
-	public void setApp(App app) {
-		this.app = app;
+	
+	@Override
+	public String toString() {
+		return "Property [name=" + name + ", label=" + label + ", creationDate=" + creationDate
+				+ ", lastUpdate=" + lastUpdate + "]";
 	}
 
 }
