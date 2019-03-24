84eca156b5c2aa08b816e23cda0ccce017cc1018
Yaison <yaison1708@gmail.com>
2019-03-23 20:33:42 -0400

    Foo data generation.

--- test/src/main/java/com/gem/test/RestTest.java
+++ test/src/main/java/com/gem/test/RestTest.java
@@ -1,19 +1,17 @@
 package com.gem.test;
 
 import java.time.Instant;
-import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalUnit;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.ws.rs.core.Response;
+
 import org.bson.Document;
-import org.bson.json.JsonMode;
-import org.bson.json.JsonWriterSettings;
 import org.bson.types.ObjectId;
 
-import com.mongodb.util.JSON;
-
+import com.gem.commons.Json;
+import com.gem.commons.rest.RestClient;
 
 /**
  * Hello world!
@@ -21,15 +19,22 @@ import com.mongodb.util.JSON;
  */
 public class RestTest {
 	public static void main(String[] args) {
+		RestClient c = new RestClient();
+		String url = "http://localhost:8080/rest/config/apps/5c8e96277aa7f6ea9c1a342f";
+		Json json = new Json();
+		json.put("name", "ddde");
+		Response r = c.put(url, json);
+		System.out.println(r);
+	}
 
-
-		String[] names = {"911", "wot", "ndmt", "netasssist", "ncct", "admin", "edx"};
+	static void test1() {
+		String[] names = { "911", "wot", "ndmt", "netasssist", "ncct", "admin", "edx" };
 
 		List<Document> list = new ArrayList<Document>();
 		
-		for(int i =0; i < names.length; i++){
+		for (int i = 0; i < names.length; i++) {
 			String name = names[i];
-	
+
 			Object id = ObjectId.get();
 			
 			Document doc = new Document();
@@ -39,24 +44,20 @@ public class RestTest {
 
 			Instant d = Instant.now();
 			d = d.minus(5 * 365, ChronoUnit.DAYS);
-			int days = (int)(Math.random()*900);
+			int days = (int) (Math.random() * 900);
 			long creation = d.plus(days, ChronoUnit.DAYS).toEpochMilli();
 
-
-			int plus = (int)(Math.random()*360);
+			int plus = (int) (Math.random() * 360);
 			long update = d.plus(plus, ChronoUnit.DAYS).toEpochMilli();
 			
 			doc.put("lastUpdate", update);
 			doc.put("creationDate", creation);
-	
-			
+
 			list.add(doc);
 		}
 		
 		Document holder = new Document();
 		holder.put("arr", list);
-		JsonWriterSettings settings = new JsonWriterSettings(JsonMode.RELAXED, true);
-		System.out.print(holder.toJson(settings));
-
+		System.out.print(holder.toJson());
 	}
 }
